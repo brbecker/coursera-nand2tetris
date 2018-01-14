ecc794ed4cc82049897251ad18e46b02ab8c0f12
Brian R. Becker <brbecker@gmail.com>
2018-01-13 20:17:03 -0800

    Unpacked new nand2tetris software. Left my updated files for projects 01 to 06 largely intact. 07 to 13 fully updated.

--- nand2tetris/projects/04/fill/Fill.asm
+++ nand2tetris/projects/04/fill/Fill.asm
@@ -5,8 +5,11 @@
 
 // Runs an infinite loop that listens to the keyboard input. 
 // When a key is pressed (any key), the program blackens the screen,
-// i.e. writes "black" in every pixel. When no key is pressed, the
-// program clears the screen, i.e. writes "white" in every pixel.
+// i.e. writes "black" in every pixel;
+// the screen should remain fully black as long as the key is pressed. 
+// When no key is pressed, the program clears the screen, i.e. writes
+// "white" in every pixel;
+// the screen should remain fully clear as long as no key is pressed.
 
 (START)
 // Initialize ptr to the start of the SCREEN
