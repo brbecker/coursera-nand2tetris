2f1dc2835b1d5c6af733a51c615dfb9e67092feb
Brian R. Becker <brbecker@gmail.com>
2018-01-15 09:45:59 -0800

    Read the file, stripping comments and external whitespace, saving remainder in a queue

--- nand2tetris/projects/07/Parser.py
+++ nand2tetris/projects/07/Parser.py
@@ -1,10 +1,25 @@
+import re
+from collections import deque
+
 class Parser:
 
     # Command types
     (C_ARITHMETIC, C_PUSH, C_POP, C_LABEL, C_GOTO, C_IF, C_FUNCTION, C_RETURN, C_CALL) = range(9)
 
+    # Regular expressions (compiled for speed)
+    COMMENT_PATTERN   = re.compile(r'//.*$')
+
     def __init__(self, filename):
-        pass
+        self._cmdqueue = deque()
+        with open(filename) as f:
+            for line in f:
+                # Strip comments and external whitespace
+                line = Parser.COMMENT_PATTERN.sub('', line).strip()
+
+                # Add to the queue if there is anything left on the line
+                if line:
+                    self._cmdqueue.append(line)
+                    # print('Added: ' + line)
 
     def hasMoreCommands(self):
         pass
