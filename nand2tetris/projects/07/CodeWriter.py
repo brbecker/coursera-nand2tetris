01a379ba8aee23c06064279ab170691389c87abc
Brian R. Becker <brbecker@gmail.com>
2018-01-15 00:56:47 -0800

    Add skeleton for Parser and CodeWriter classes

---
+++ nand2tetris/projects/07/CodeWriter.py
@@ -0,0 +1,13 @@
+class CodeWriter:
+
+    def __init__(self, filename):
+        pass
+
+    def setFileName(self, filename):
+        pass
+
+    def writeArithmetic(self, command):
+        pass
+
+    def writePushPop(self, command, segment, index):
+        pass
---
+++ nand2tetris/projects/07/Parser.py
@@ -0,0 +1,19 @@
+class Parser:
+
+    def __init__(self, filename):
+        pass
+
+    def hasMoreCommands(self):
+        pass
+
+    def advance(self):
+        pass
+
+    def commandType(self):
+        pass
+
+    def arg1(self):
+        pass
+
+    def arg2(self):
+        pass
