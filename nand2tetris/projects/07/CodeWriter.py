239f91108f221fb69dbf47444e809e33383e9d00
Brian R. Becker <brbecker@gmail.com>
2018-01-16 14:58:29 -0800

    Move indentation into writeCode, since most lines will have it. Move blank line generation to end of command, since all branches will have it.

--- nand2tetris/projects/07/CodeWriter.py
+++ nand2tetris/projects/07/CodeWriter.py
@@ -20,24 +20,29 @@ class CodeWriter:
         # push constant 7
         if segment == 'constant':
             if command == Parser.C_PUSH:
-                self.writeCode('    @{0}'.format(index))
-                self.writeCode('    D=A')
-                self.writeCode('    @SP')
-                self.writeCode('    A=M')
-                self.writeCode('    M=D')
-                self.writeCode('    @SP')
-                self.writeCode('    M=M+1')
-                self.writeCode('')
+                self.writeCode('@{0}'.format(index))
+                self.writeCode('D=A')
+                self.writeCode('@SP')
+                self.writeCode('A=M')
+                self.writeCode('M=D')
+                self.writeCode('@SP')
+                self.writeCode('M=M+1')
             else:
                 print("ERROR: pop not supported for constant segment: " + cmdtext)
         else:
             print("WARNING: Unimplemented segment: " + segment)
 
+        # Create a blank line in the ASM output after each VM command
+        self.writeCode('', indent=False)
+
     def writeComment(self, vmfilename, cmdtext, lineno):
-        self.writeCode('// {0} [{2}]: {1}'.format(vmfilename, cmdtext, lineno))
+        self.writeCode('// {0} [{2}]: {1}'.format(vmfilename, cmdtext, lineno),
+                       indent=False)
 
     # Simplify code generation by putting file specification in one place
-    def writeCode(self, code):
+    def writeCode(self, code, indent=True):
+        if indent:
+            code = '    ' + code
         print(code, file=self._outfile)
 
     def close(self):
