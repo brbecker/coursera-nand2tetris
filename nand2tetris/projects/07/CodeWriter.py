237c395262b67590945b03c5ac1cf10ee8afda79
Brian R. Becker <brbecker@gmail.com>
2018-01-15 22:50:08 -0800

    Output comments with the source commands and location

--- nand2tetris/projects/07/CodeWriter.py
+++ nand2tetris/projects/07/CodeWriter.py
@@ -6,11 +6,17 @@ class CodeWriter:
     def setFileName(self, filename):
         self._vmfile = filename
 
-    def writeArithmetic(self, command):
-        pass
+    def writeArithmetic(self, command, cmdtext=None, lineno=None):
+        if cmdtext and lineno:
+            self.writeComment(self._vmfile, cmdtext, lineno)
 
-    def writePushPop(self, command, segment, index):
-        pass
+    def writePushPop(self, command, segment, index, cmdtext=None, lineno=None):
+        if cmdtext and lineno:
+            self.writeComment(self._vmfile, cmdtext, lineno)
+
+    def writeComment(self, vmfilename, cmdtext, lineno):
+        print('// {0}[{2}]: {1}'.format(vmfilename, cmdtext, lineno),
+              file=self._outfile)
 
     def close(self):
         close(self._outfile)
