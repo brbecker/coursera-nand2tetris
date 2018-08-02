brbecker/coursera-nand2tetris

commit 15068ae0927010cb448cd343274ba5408ccbdfc1

Factor the comment and blank line per VM command into their own functions and let VMTranslator call them, simplifying the write* functions

nand2tetris/projects/08/CodeWriter.py
@@ -19,10 +19,7 @@ class CodeWriter:
         # "function" is encountered.
         self._currFunction = '_null'
 
-    def writeArithmetic(self, command, cmdtext=None, lineno=None):
-        if cmdtext and lineno:
-            self.writeComment(self._vmfile, cmdtext, lineno)
-
+    def writeArithmetic(self, command):
         # Set command to lower case
         command = command.lower()
 
@@ -70,13 +67,7 @@ class CodeWriter:
             else:
                 print("WARNING: Unimplemented arithmetic command: " + command)
 
-        # Create a blank line in the ASM output after each VM command
-        self.writeCode('', indent=False)
-
-    def writePushPop(self, ctype, segment, index, cmdtext=None, lineno=None):
-        if cmdtext and lineno:
-            self.writeComment(self._vmfile, cmdtext, lineno)
-
+    def writePushPop(self, ctype, segment, index):
         # Set segment to lower case
         segment = segment.lower()
 
@@ -166,34 +157,16 @@ class CodeWriter:
         else:
             raise ValueError('writePushPop: Unrecognized ctype {0}'.format(ctype))
 
-        # Create a blank line in the ASM output after each VM command
-        self.writeCode('', indent=False)
-
-    def writeLabel(self, label, cmdtext=None, lineno=None):
-        if cmdtext and lineno:
-            self.writeComment(self._vmfile, cmdtext, lineno)
-
+    def writeLabel(self, label):
         # Output the ASM label
         self.writeCode('({0}${1})'.format(self._currFunction, label), indent=False)
 
-        # Create a blank line in the ASM output after each VM command
-        self.writeCode('', indent=False)
-
-    def writeGoto(self, label, cmdtext=None, lineno=None):
-        if cmdtext and lineno:
-            self.writeComment(self._vmfile, cmdtext, lineno)
-
+    def writeGoto(self, label):
         # Load the destination and jump
         self.writeCode('@{0}${1}'.format(self._currFunction, label))
         self.writeCode('0;JMP')
 
-        # Create a blank line in the ASM output after each VM command
-        self.writeCode('', indent=False)
-
-    def writeIf(self, label, cmdtext=None, lineno=None):
-        if cmdtext and lineno:
-            self.writeComment(self._vmfile, cmdtext, lineno)
-
+    def writeIf(self, label):
         # Pop the top of the stack into D
         self.writeCode('@SP')
         self.writeCode('AM=M-1')
@@ -203,12 +176,12 @@ class CodeWriter:
         self.writeCode('@{0}${1}'.format(self._currFunction, label))
         self.writeCode('D;JNE')
 
-        # Create a blank line in the ASM output after each VM command
-        self.writeCode('', indent=False)
-
-    def writeComment(self, vmfilename, cmdtext, lineno):
-        self.writeCode('// {0} [{2}]: {1}'.format(vmfilename, cmdtext, lineno),
+    def writeComment(self, cmdtext, lineno):
+        self.writeCode('// {0} [{2}]: {1}'.format(self._vmfile, cmdtext, lineno),
                        indent=False)
+
+    def writeBlank(self):
+        self.writeCode('', indent=False)
 
     # Simplify code generation by putting file specification in one place
     def writeCode(self, code, indent=True):

nand2tetris/projects/08/VMTranslator.py
@@ -40,27 +40,28 @@ for vmfile in vmfiles:
         # Get the command type
         ctype = parser.commandType()
 
+        # Write a comment into the ASM file with the VM command
+        cw.writeComment(parser.command(), parser.lineno())
+
         # Generate the code for the command
         if ctype == Parser.C_ARITHMETIC:
-            cw.writeArithmetic(parser.arg1(),
-                               parser.command(), parser.lineno())
+            cw.writeArithmetic(parser.arg1())
         elif ctype == Parser.C_PUSH or ctype == Parser.C_POP:
-            cw.writePushPop(ctype, parser.arg1(), parser.arg2(),
-                            parser.command(), parser.lineno())
+            cw.writePushPop(ctype, parser.arg1(), parser.arg2())
         elif ctype == Parser.C_LABEL:
-            cw.writeLabel(parser.arg1(),
-                          parser.command(), parser.lineno())
+            cw.writeLabel(parser.arg1())
         elif ctype == Parser.C_GOTO:
-            cw.writeGoto(parser.arg1(),
-                         parser.command(), parser.lineno())
+            cw.writeGoto(parser.arg1())
         elif ctype == Parser.C_IF:
-            cw.writeIf(parser.arg1(),
-                       parser.command(), parser.lineno())
+            cw.writeIf(parser.arg1())
         elif ctype in range(len(Parser.CMDS)):
             print("WARNING: Unimplemented ctype: " + str(ctype))
         else:
             print("ERROR: Unrecognized ctype: " + str(ctype))
             sys.exit(1)
+
+        # Write a blank line into the ASM file after each VM command
+        cw.writeBlank()
 
 # Close the CodeWriter
 cw.close()

commit c3b15f085fc460ce8d4f879f383ef20686570af9

Remove references to obsolete cmdtext argument in writePushPop(). Error introduced in 15068ae0927010cb448cd343274ba5408ccbdfc1.

nand2tetris/projects/08/CodeWriter.py
@@ -81,11 +81,11 @@ class CodeWriter:
 
         # Error checking
         if ctype == Parser.C_POP and segment == 'constant':
-            raise ValueError('pop not supported for constant segment: ' + cmdtext)
+            raise ValueError('pop not supported for constant segment')
         if segment == 'pointer' and int(index) not in range(0, 2):
-            raise ValueError('pointer segment only supports indexes 0 and 1: ' + cmdtext)
+            raise ValueError('pointer segment only supports indexes 0 and 1')
         if segment == 'temp' and int(index) not in range(0, 8):
-            raise ValueError('temp segment only supports indexes from 0 to 7: ' + cmdtext)
+            raise ValueError('temp segment only supports indexes from 0 to 7')
 
         # Calculate the RAM address we really want
 